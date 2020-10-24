01ed99f4f554f33474d2ee218f9007f7ded36346
Răzvan-Gabriel Geangu <razvan.geangu@gmail.com>
2020-10-24 19:26:25 +0100

    feat: add labels and workflow

--- flamingo/flamingo/AR/CardIOViewController.swift
+++ flamingo/flamingo/AR/CardIOViewController.swift
@@ -12,55 +12,55 @@ class CardIOViewController: UIViewController, CardIOViewDelegate {
     var captureSession: AVCaptureSession!
     var stillImageOutput: AVCapturePhotoOutput!
     var videoPreviewLayer: AVCaptureVideoPreviewLayer!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         CardIOUtilities.canReadCardWithCamera()
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         CardIOUtilities.preloadCardIO()
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
-        prepareCameraBackground()
-
+//        prepareCameraBackground()
+        
         let cardIOView = CardIOView(frame: self.view.frame)
         cardIOView.delegate = self
         self.view.addSubview(cardIOView)
     }
-
+    
     func prepareCameraBackground() {
         captureSession = AVCaptureSession()
-        captureSession.sessionPreset = .low
-
-        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
-            else {
-                print("Unable to access back camera!")
-                return
+        captureSession.sessionPreset = .photo
+        
+        guard let backCamera = AVCaptureDevice.default(for: .video) else {
+            print("Unable to access back camera!")
+            return
         }
-
-        do {
-            let input = try AVCaptureDeviceInput(device: backCamera)
+        
+        guard let input = try? AVCaptureDeviceInput(device: backCamera) else {
+            print("Error Unable to initialize back camera")
+            return
         }
-        catch let error  {
-            print("Error Unable to initialize back camera:  \(error.localizedDescription)")
-        }
-
+        
         stillImageOutput = AVCapturePhotoOutput()
-
+        
         if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput) {
             captureSession.addInput(input)
             captureSession.addOutput(stillImageOutput)
-            setupLivePreview()
+            
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            
+            videoPreviewLayer.videoGravity = .resizeAspectFill
+            videoPreviewLayer.connection?.videoOrientation = .portrait
+            self.view.layer.addSublayer(videoPreviewLayer)
+            
+            let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .regular))
+            blurView.frame = self.view.bounds
+            self.view.addSubview(blurView)
         }
-
-        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        
-        videoPreviewLayer.videoGravity = .resizeAspect
-        videoPreviewLayer.connection?.videoOrientation = .portrait
-        view.layer.addSublayer(videoPreviewLayer)
         
         DispatchQueue.global(qos: .userInitiated).async {
             self.captureSession.startRunning()
@@ -69,7 +69,7 @@ class CardIOViewController: UIViewController, CardIOViewDelegate {
             }
         }
     }
-
+    
     func cardIOView(_ cardIOView: CardIOView!, didScanCard cardInfo: CardIOCreditCardInfo!) {
         print(cardInfo.cardNumber ?? "")
     }
